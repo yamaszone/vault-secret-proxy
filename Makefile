ee8984fab2915f8a5742c7920d0eb2e8120387da
Mazedur Rahman <mazedur.rahman.liton@gmail.com>
2019-03-02 18:27:22 -0800

    Vault secret proxy server with dummy payload

---
+++ Makefile
@@ -0,0 +1,15 @@
+install:
+	go install -v ./cmd/vault-secrets-proxy-server/
+
+validate:
+	swagger validate ./swagger/swagger.yml
+
+gen: validate
+	swagger generate server \
+		--target=. \
+		--spec=./swagger/swagger.yml \
+		--name=vault-secrets-proxy
+
+
+# just added `gen` and `validate`
+.PHONY: install gen validate
---
+++ restapi/configure_vault_secrets_proxy.go
@@ -0,0 +1,81 @@
+// This file is safe to edit. Once it exists it will not be overwritten
+
+package restapi
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	errors "github.com/go-openapi/errors"
+	runtime "github.com/go-openapi/runtime"
+	middleware "github.com/go-openapi/runtime/middleware"
+
+	"vault-secret-proxy/restapi/operations"
+)
+
+//go:generate swagger generate server --target ../../vault-secret-proxy --name VaultSecretsProxy --spec ../swagger/swagger.yml
+
+func configureFlags(api *operations.VaultSecretsProxyAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+}
+
+func configureAPI(api *operations.VaultSecretsProxyAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...interface{})
+	//
+	// Example:
+	api.Logger = log.Printf
+
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.JSONProducer = runtime.JSONProducer()
+
+	api.GetSecretsHandler = operations.GetSecretsHandlerFunc(func(params operations.GetSecretsParams) middleware.Responder {
+		jsonBytes := []byte(
+			`{
+				"key1":"val1",
+				"key2":"val2"
+			}`)
+
+		var response interface{}
+		err := json.Unmarshal(jsonBytes, &response)
+		if err != nil {
+			api.Logger("Error parsing JSON ", err)
+		}
+
+		return operations.NewGetSecretsOK().WithPayload(response)
+	})
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix"
+func configureServer(s *http.Server, scheme, addr string) {
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
